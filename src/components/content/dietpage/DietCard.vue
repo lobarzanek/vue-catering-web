b06eaff5e3ba53beac840b5c8e1ce74aa3e4456a
b0r55uk <101877993+b0r55uk@users.noreply.github.com>
2023-09-25 13:56:19 +0200

    add css reset

---
+++ src/components/content/dietpage/DietCard.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: () => "",
+  },
+  description: {
+    type: String,
+    default: () => "",
+  },
+  mealsCount: {
+    type: Number,
+    default: () => "",
+  },
+  price: {
+    type: Number,
+    default: () => "",
+  },
+  currency: {
+    type: String,
+    default: () => "",
+  },
+  imgSrc: {
+    type: String,
+    default: () => "",
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-content front-card">
+      <span class="title">{{ title }}</span>
+    </div>
+    <div class="card-content back-card">
+      <span class="description">{{ description }}</span>
+      <span class="mealsCount">{{ mealsCount }}</span>
+      <span class="price">{{ price }}</span>
+      <span class="currency">{{ currency }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;1,700&display=swap");
+.card {
+  font-family: "Nunito", sans-serif;
+  width: 250px;
+  height: 350px;
+  border-radius: 8px;
+  box-shadow: 2px 4px 10px 1px #01111170;
+  overflow: hidden;
+  
+}
+</style>
--- src/views/DietView.vue
+++ src/views/DietView.vue
@@ -1,7 +1,68 @@
+<script setup>
+import { ref } from "vue";
+import DietCard from "../components/content/dietpage/DietCard.vue";
+
+const data = ref([]);
+data.value = [
+  {
+    id: 1,
+    title: "Dieta 1500kcal",
+    description: "dieta dla małych",
+    mealsCount: 5,
+    price: 100,
+    currency: "PLN",
+  },
+  {
+    id: 2,
+    title: "Dieta 2000kcal",
+    description: "dieta dla średnich",
+    mealsCount: 5,
+    price: 200,
+    currency: "PLN",
+  },
+  {
+    id: 3,
+    title: "Dieta 2500kcal",
+    description: "dieta dla dużych",
+    mealsCount: 5,
+    price: 300,
+    currency: "PLN",
+  },
+];
+</script>
+
 <template>
-<h1>dieta</h1>
+  <div class="dietView">
+    <h1>dieta</h1>
+    <div class="dietCards">
+      <DietCard
+        v-for="(item, index) in data"
+        :title="item.title"
+        :description="item.description"
+        :meals-count="item.mealsCount"
+        :price="item.price"
+        :currency="item.currency"
+        :key="index"
+      ></DietCard>
+    </div>
+  </div>
 </template>
 
-<style>
-
+<style scoped lang="scss">
+.dietView {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .dietCards {
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin: 30px 0;
+    gap: 100px;
+  }
+}
 </style>
