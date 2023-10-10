e2f499fa6a773411365e8f43629d9425ce9db52c
b0r55uk <lukiob@gmail.com>
2023-10-10 17:59:47 +0200

    Add transition to router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Navbar from './components/menu/Navbar.vue'
-import HomeView from './views/HomeView.vue';
+import { RouterView } from "vue-router";
+import Navbar from "./components/menu/Navbar.vue";
 </script>
 
 <template>
-  <Navbar/>
-  <RouterView/>
+  <Navbar></Navbar>
+  <RouterView v-slot="{ Component }">
+    <Transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>
 </template>
 
 <style scoped>
-
+.route-enter-from{
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active{
+  transition: all .3s ease-out;
+}
+.route-leave-to{
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active{
+  transition: all .3s ease-in;
+}
 </style>
