c97a3ecae4424a5c8a8320b89ca97fd16d60ee01
b0r55uk <lukiob@gmail.com>
2023-10-10 19:00:13 +0200

    Correct typo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from "vue-router";
-import Navbar from "./components/menu/Navbar.vue";
+import Navbar from "@/components/menu/Navbar.vue";
 </script>
 
 <template>
--- src/views/DietView.vue
+++ src/views/DietView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script async setup>
 import { ref } from "vue";
 import axios from "axios";
 import DietCard from "../components/content/dietpage/DietCard.vue";
@@ -7,7 +7,8 @@ import ErrorHandler from "@/components/content/errorHandler/ErrorHandler.vue";
 const data = ref([]);
 const eHandler = ref(true);
 
-const getDietsData = () => {
+const getDietsData = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   axios.get("http://localhost:3000/diets").then((response) => {
     if (response.statusText == "OK") {
       eHandler.value = false;
@@ -19,26 +20,31 @@ getDietsData();
 </script>
 
 <template>
-  <div class="errorHandler" v-if="eHandler">
+  <!-- <div class="errorHandler" v-if="eHandler">
     <ErrorHandler></ErrorHandler>
-  </div>
-  <div class="dietView" v-else>
-    <h1>Nasze diety:</h1>
-    <div class="dietCards">
-      <DietCard
-        v-for="(item, index) in data"
-        :id="item.id"
-        :title="item.title"
-        :description="item.description"
-        :meals-count="item.mealsCount"
-        :price="item.price"
-        :currency="item.currency"
-        :imgSrc="item.imgSrc"
-        :imgAlt="item.imgAlt"
-        :key="index"
-      ></DietCard>
-    </div>
-  </div>
+  </div> -->
+  <Suspense>
+    <template #default>
+      <div class="dietView">
+        <h1>Nasze diety:</h1>
+        <div class="dietCards">
+          <DietCard
+            v-for="(item, index) in data"
+            :id="item.id"
+            :title="item.title"
+            :description="item.description"
+            :meals-count="item.mealsCount"
+            :price="item.price"
+            :currency="item.currency"
+            :imgSrc="item.imgSrc"
+            :imgAlt="item.imgAlt"
+            :key="index"
+          ></DietCard>
+        </div>
+      </div>
+    </template>
+    <template #fallback> Loading... </template>
+  </Suspense>
 </template>
 
 <style scoped lang="scss">
